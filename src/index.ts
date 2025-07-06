be8b76f160404c95ec6f06d12e6b1cf39e58cee3
David Warrington <9138568+davidwarrington@users.noreply.github.com>
2025-07-06 13:23:33 +0100

    feat(plugin): create (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-// eslint-disable-next-line unicorn/no-empty-file
+import type { Plugin } from 'postcss';
+import selectorParser, { pseudo } from 'postcss-selector-parser';
+
+const PSEUDO = ':focus-visible-within';
+const REPLACEMENT = ':has(:focus-visible)';
+
+export default function postcssFocusVisibleWithin(): Plugin {
+  return {
+    postcssPlugin: 'postcss-focus-visible-within',
+
+    Rule(rule) {
+      if (!rule.selector.toLowerCase().includes(PSEUDO)) {
+        return;
+      }
+
+      selectorParser(root => {
+        root.walkPseudos(node => {
+          if (node.value !== PSEUDO) {
+            return;
+          }
+
+          node.replaceWith(pseudo({ value: REPLACEMENT }));
+        });
+      }).transformSync(rule, {
+        lossless: true,
+        updateSelector: true,
+      });
+    },
+  };
+}
